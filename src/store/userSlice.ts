0a53b8a18cc73cb1834c550e3d226131fa5d5e39
Neuter <mark@air.city>
2025-01-16 18:29:47 +0800

    Update userSlice.ts

--- src/store/userSlice.ts
+++ src/store/userSlice.ts
@@ -3,11 +3,13 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 interface UserState {
   selectedCarId: number | null;
   selectedStationId: number | null;
+  viewState: 'showCar' | 'showMap';  // Added view state
 }
 
 const initialState: UserState = {
   selectedCarId: null,
-  selectedStationId: null
+  selectedStationId: null,
+  viewState: 'showCar'  // Default to car view
 };
 
 const userSlice = createSlice({
@@ -20,16 +22,21 @@ const userSlice = createSlice({
     selectStation: (state, action: PayloadAction<number>) => {
       state.selectedStationId = action.payload;
     },
+    setViewState: (state, action: PayloadAction<'showCar' | 'showMap'>) => {
+      state.viewState = action.payload;
+    },
     resetBooking: (state) => {
       state.selectedCarId = null;
       state.selectedStationId = null;
+      // Note: doesn't reset viewState as that's independent
     }
   }
 });
 
-export const { selectCar, selectStation, resetBooking } = userSlice.actions;
+export const { selectCar, selectStation, setViewState, resetBooking } = userSlice.actions;
 export default userSlice.reducer;
 
 // Selectors
 export const selectSelectedCarId = (state: { user: UserState }) => state.user.selectedCarId;
 export const selectSelectedStationId = (state: { user: UserState }) => state.user.selectedStationId;
+export const selectViewState = (state: { user: UserState }) => state.user.viewState;
