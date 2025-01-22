abce32d51954b0d741b865bf6cf4b0ef409b867b
Neuter <mark@air.city>
2025-01-22 09:55:32 +0800

    Create page.tsx

---
+++ src/app/signin/page.tsx
@@ -0,0 +1,11 @@
+import SignIn from '@/components/ui/signin';
+
+export default function SignInPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-background">
+      <div className="w-full max-w-md">
+        <SignIn />
+      </div>
+    </div>
+  );
+}
