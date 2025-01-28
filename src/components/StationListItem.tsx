a0380409a097583ac99282140ef2d59427c8efd6
Neuter <mark@air.city>
2025-01-28 09:35:30 +0800

    Update StationListItem.tsx

--- src/components/StationListItem.tsx
+++ src/components/StationListItem.tsx
@@ -14,7 +14,7 @@ interface StationListItemProps extends ListChildComponentProps {
   data: StationFeature[];
 }
 
-const StationListItem = memo<StationListItemProps>((props) => {
+export const StationListItem = memo<StationListItemProps>((props) => {
   const { index, style, data } = props;
   const station = data[index];
   const dispatch = useAppDispatch();
@@ -59,5 +59,3 @@ const StationListItem = memo<StationListItemProps>((props) => {
 });
 
 StationListItem.displayName = 'StationListItem';
-
-export default StationListItem;
