88dc7ab424f8b239587ba0446aa6ce7a6dfb6658
Neuter <mark@air.city>
2025-02-09 15:25:52 +0800

    Update map.ts

--- src/constants/map.ts
+++ src/constants/map.ts
@@ -210,7 +210,7 @@ const userPinPath = `
     station: {
       path: hexagonPath,
       scale: 9,
-      fillColor: '#6B7280',   // Gray (Tailwind gray-500)
+      fillColor: '#F7FAFA',   // Gray (Tailwind gray-500)
       fillOpacity: 1,
       strokeWeight: 2,
       strokeColor: '#FFFFFF',
