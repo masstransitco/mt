6651b408011ad807892f26a5d75fe9ce7621cf71
Neuter <mark@air.city>
2025-02-13 12:29:00 +0800

    Update StationSelector.tsx

--- src/components/StationSelector.tsx
+++ src/components/StationSelector.tsx
@@ -312,7 +312,7 @@ export default function StationSelector({
             selectedStation={departureStation}
           />
 
-          {departureStation && (
+          {departureStation && step < 3 && (
             <button
               onClick={() => {
                 // Instead of dispatching directly here, we call the parent's callback.
