42a1a55ac4b6c814e74f2d9b25a5be82e42033ba
Neuter <mark@air.city>
2025-02-15 01:11:48 +0800

    Update GMap.tsx

--- src/components/GMap.tsx
+++ src/components/GMap.tsx
@@ -188,34 +188,47 @@ export default function GMap({ googleApiKey }: GMapProps) {
         clearInterval(intervalId);
         const canvas = renderer.domElement;
         canvas.style.pointerEvents = "auto";
+        canvas.style.zIndex = "1";
 
         const raycaster = new THREE.Raycaster();
 
         const onCanvasClick = (event: MouseEvent) => {
-          const rect = canvas.getBoundingClientRect();
-          const mouse = new THREE.Vector2(
-            ((event.clientX - rect.left) / rect.width) * 2 - 1,
-            -((event.clientY - rect.top) / rect.height) * 2 + 1
-          );
-          raycaster.setFromCamera(mouse, camera);
-          const intersections = raycaster.intersectObjects(stationCubesRef.current, true);
-          console.log("Intersections:", intersections);
-          if (intersections.length > 0) {
-            const intersected = intersections[0].object;
-            const station = intersected.userData.station;
-            if (station) {
-              handleStationClick(station);
-            }
-          }
-        };
+  const rect = canvas.getBoundingClientRect();
+  const mouse = new THREE.Vector2(
+    ((event.clientX - rect.left + window.scrollX) / rect.width) * 2 - 1,
+    -((event.clientY - rect.top + window.scrollY) / rect.height) * 2 + 1
+  );
+  
+  console.log("Click coordinates:", { x: event.clientX, y: event.clientY });
+  console.log("Normalized mouse coords:", mouse);
+  console.log("Camera:", { 
+    position: camera.position,
+    rotation: camera.rotation
+  });
+  
+  raycaster.setFromCamera(mouse, camera);
+  const intersections = raycaster.intersectObjects(stationCubesRef.current, true);
+  
+  console.log("Number of cubes:", stationCubesRef.current.length);
+  console.log("Intersections found:", intersections.length);
+  
+  if (intersections.length > 0) {
+    const intersected = intersections[0].object;
+    console.log("Intersected object:", intersected);
+    const station = intersected.userData.station;
+    if (station) {
+      handleStationClick(station);
+    }
+  }
+};
 
         const onCanvasTouchEnd = (event: TouchEvent) => {
           const touch = event.changedTouches[0];
           const rect = canvas.getBoundingClientRect();
           const mouse = new THREE.Vector2(
-            ((touch.clientX - rect.left) / rect.width) * 2 - 1,
-            -((touch.clientY - rect.top) / rect.height) * 2 + 1
-          );
+  ((event.clientX - rect.left + window.scrollX) / rect.width) * 2 - 1,
+  -((event.clientY - rect.top + window.scrollY) / rect.height) * 2 + 1
+);
           raycaster.setFromCamera(mouse, camera);
           const intersections = raycaster.intersectObjects(stationCubesRef.current, true);
           console.log("Intersections (touch):", intersections);
