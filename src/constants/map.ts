79e2029ff3dbeeb9166c4c0cfb3a9061234e44c7
Neuter <mark@air.city>
2025-02-18 13:56:06 +0800

    Update map.ts

--- src/constants/map.ts
+++ src/constants/map.ts
@@ -138,7 +138,7 @@ export const createMapOptions = (): google.maps.MapOptions => ({
   minZoom: 6,
   clickableIcons: false,
   styles: MAP_STYLES,
-  mapId: 'c7d03d22763bc313',
+  mapId: 'e18c09caf6c67ab3',
   restriction: {
     latLngBounds: HK_BOUNDS,
     strictBounds: true,
