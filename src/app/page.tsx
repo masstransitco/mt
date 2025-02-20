3f43dc25093bdc8a3d70ea7056138e26de01cf4b
Neuter <mark@air.city>
2025-02-20 11:52:38 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState, useCallback } from 'react';
 import Head from 'next/head';
-import { Menu as MenuIcon, ScanLine } from 'lucide-react';
+import { Menu as MenuIcon } from 'lucide-react';
 import Image from 'next/image';
 
 import dynamic from 'next/dynamic'; // <-- Import dynamic
@@ -60,7 +60,16 @@ export default function Page() {
                 onClick={() => setIsScannerOpen(true)}
                 className="flex items-center justify-center w-8 h-8 rounded-full text-gray-400 hover:text-gray-200 hover:bg-gray-700 active:bg-gray-600 transition-colors"
               >
-                <ScanLine className="w-5 h-5" />
+                {/* Custom QR Code Icon */}
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#000000" fill="none">
+                  <path d="M18 15L18 18H14L14 15L18 15Z" fill="currentColor" />
+                  <path fillRule="evenodd" clipRule="evenodd" d="M1.5 1.49994H9.1306V3.40903H3.40909V9.13054H1.5V1.49994ZM20.5909 3.40903H14.8694V1.49994H22.5V9.13054H20.5909V3.40903ZM22.5 14.8693V22.4999H14.8694V20.5908H20.5909V14.8693H22.5ZM1.5 14.8693H3.40909V20.5908H9.1306V22.4999H1.5V14.8693Z" fill="currentColor" />
+                  <path fillRule="evenodd" clipRule="evenodd" d="M12 12V6H14V13C14 13.5523 13.5523 14 13 14H6V12H12Z" fill="currentColor" />
+                  <path d="M10 6L10 10H6L6 6L10 6Z" fill="currentColor" />
+                  <path fillRule="evenodd" clipRule="evenodd" d="M11 18H6V16H11V18Z" fill="currentColor" />
+                  <path fillRule="evenodd" clipRule="evenodd" d="M16 12L16 10L18 10L18 12L16 12Z" fill="currentColor" />
+                  <path fillRule="evenodd" clipRule="evenodd" d="M16 8L16 6L18 6L18 8L16 8Z" fill="currentColor" />
+                </svg>
               </button>
               {/* Vertical Divider */}
               <div className="w-px h-6 bg-gray-500" />
