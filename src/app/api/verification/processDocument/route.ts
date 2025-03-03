3529b37c16736f519b5f935543c647f477b34c3c
Neuter <mark@air.city>
2025-03-03 10:21:54 +0800

    Update route.ts

--- src/app/api/verification/processDocument/route.ts
+++ src/app/api/verification/processDocument/route.ts
@@ -1,6 +1,6 @@
 // /src/app/api/verification/processDocument/route.ts
 
-import tencentcloud from "tencentcloud-sdk-nodejs";
+import tencentcloud from "tencentcloud-sdk-nodejs-intl-en";
 import { Storage } from "@google-cloud/storage";
 import { db } from "@/lib/firebase-admin";
 import { NextResponse } from "next/server";
