befa7fedc31d884345306f99329003cdd0d90d09
Neuter <mark@air.city>
2025-03-03 17:20:20 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,23 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['api.placeholder.com'],
+    domains: ["api.placeholder.com"],
   },
   reactStrictMode: true,
   async rewrites() {
     return [
+      // 1) Let /api/... requests pass through without rewriting
       {
-        source: '/:path*',
+        source: "/api/:path*",
+        destination: "/api/:path*",
+      },
+
+      // 2) Rewrite everything else if the host is admin.masstransitcar.com
+      {
+        source: "/:path*",
         has: [
           {
-            type: 'host',
-            value: 'admin.masstransitcar.com',
+            type: "host",
+            value: "admin.masstransitcar.com",
           },
         ],
-        destination: '/admin/:path*',
+        destination: "/admin/:path*",
       },
     ];
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
