81dcb8e1bd28218e654e3a7702f1d487c4d59c2f
Neuter <mark@air.city>
2025-03-04 21:00:02 +0800

    Update GMap.tsx

--- src/components/GMap.tsx
+++ src/components/GMap.tsx
@@ -579,56 +579,56 @@ export default function GMap({ googleApiKey }: GMapProps) {
 
           {/* Station Detail Sheet */}
           <Sheet
-            key={detailKey}
-            isOpen={(openSheet === "detail" || forceSheetOpen) && !!stationToShow}
-            onDismiss={() => {
-              // With our updated Sheet, this is only called from the X button
-              requestAnimationFrame(() => {
-                closeCurrentSheet();
-                if (overlayRef.current?.requestRedraw) {
-                  overlayRef.current.requestRedraw();
-                }
-              });
-            }}
-            onClearSelection={() => {
-              // This is the X button handler
-              requestAnimationFrame(() => {
-                if (bookingStep <= 2) {
-                  handleClearDepartureInSelector();
-                } else {
-                  handleClearArrivalInSelector();
-                }
-              });
-            }}
-            title={getSheetTitle()}
-            subtitle={getSheetSubtitle()}
-          >
-            {stationToShow && (
-              <StationDetail
-                key={detailKey}
-                stations={searchLocation ? sortedStations : stations}
-                activeStation={stationToShow}
-                onOpenSignIn={handleOpenSignIn}
-                onOpenWalletModal={() => setIsSplatModalOpen(true)}
-                onDismiss={() => {
-                  // This is only called when the sheet actually needs to close
-                  requestAnimationFrame(() => {
-                    closeCurrentSheet();
-                  });
-                }}
-                onClearStation={() => {
-                  // This is only triggered when we want to clear the station
-                  requestAnimationFrame(() => {
-                    if (bookingStep <= 2) {
-                      handleClearDepartureInSelector();
-                    } else {
-                      handleClearArrivalInSelector();
-                    }
-                  });
-                }}
-              />
-            )}
-          </Sheet>
+  key={detailKey}
+  isOpen={(openSheet === "detail" || forceSheetOpen) && !!stationToShow}
+  onDismiss={() => {
+    // This will now only be called after onClearSelection finishes
+    requestAnimationFrame(() => {
+      closeCurrentSheet();
+      if (overlayRef.current?.requestRedraw) {
+        overlayRef.current.requestRedraw();
+      }
+    });
+  }}
+  onClearSelection={() => {
+    // In step 4, ensure we properly clean up payment state before clearing the station
+    if (bookingStep === 4) {
+      // Clear arrival station - must happen before UI is removed
+      dispatch(clearArrivalStation());
+      dispatch(advanceBookingStep(3)); 
+      dispatch(clearRoute());
+    } else if (bookingStep <= 2) {
+      dispatch(clearDepartureStation());
+      dispatch(advanceBookingStep(1));
+      dispatch(clearDispatchRoute());
+    }
+    // Let the system know we've properly handled the state changes
+    toast.success(bookingStep <= 2 
+      ? "Departure station cleared. (Back to selecting departure.)" 
+      : "Arrival station cleared. (Back to selecting arrival.)");
+  }}
+  title={getSheetTitle()}
+  subtitle={getSheetSubtitle()}
+>
+  {stationToShow && (
+    <StationDetail
+      key={detailKey}
+      stations={searchLocation ? sortedStations : stations}
+      activeStation={stationToShow}
+      onOpenSignIn={handleOpenSignIn}
+      onOpenWalletModal={() => setIsSplatModalOpen(true)}
+      onDismiss={closeCurrentSheet}
+      onClearStation={() => {
+        // This function will be called from within StationDetail
+        if (bookingStep <= 2) {
+          handleClearDepartureInSelector();
+        } else {
+          handleClearArrivalInSelector();
+        }
+      }}
+    />
+  )}
+</Sheet>
           {/* GaussianSplatModal */}
           <Suspense fallback={<div>Loading modal...</div>}>
             {isSplatModalOpen && (
