5c1f6eb0bf3b235f12470a6ae8f892a08db01d29
Neuter <mark@air.city>
2025-03-05 19:41:52 +0800

    Update store.ts

--- src/store/store.ts
+++ src/store/store.ts
@@ -10,6 +10,10 @@ import bookingReducer from './bookingSlice';
 import stations3DReducer from './stations3DSlice';
 import dispatchReducer from './dispatchSlice';
 
+// 1) Import your new verification reducer
+import verificationReducer from './verificationSlice';
+
+// 2) Create the Redux store, adding verificationReducer
 export const store = configureStore({
   reducer: {
     chat: chatReducer,
@@ -19,15 +23,17 @@ export const store = configureStore({
     stations3D: stations3DReducer,
     car: carReducer,
     booking: bookingReducer,
-    dispatch: dispatchReducer,  // Add the dispatch reducer
+    dispatch: dispatchReducer,
+    verification: verificationReducer, // Add the verification slice
   },
 });
 
-// 3) Types for use in components and hooks
+// 3) Provide type helpers for use in components/hooks
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 
+// Typed versions of useDispatch/useSelector
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
