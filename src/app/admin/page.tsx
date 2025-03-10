6af957d5ceeffbfa453cbb8750a3c851c9806004
Neuter <mark@air.city>
2025-03-10 14:44:18 +0800

    Delete src/app/admin/page.tsx

--- src/app/admin/page.tsx
+++
@@ -1,8 +0,0 @@
-// src/app/admin/page.tsx
-"use client";
-
-import VerificationAdmin from '@/components/admin/VerificationAdmin';
-
-export default function AdminPage() {
-  return <VerificationAdmin />;
-}
