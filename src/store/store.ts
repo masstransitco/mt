ca0b39d3f4e36b6fa7d0ee40dfb3444e87219e3b
Neuter <mark@air.city>
2025-03-10 14:09:56 +0800

    Update store.ts

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,50 +1,54 @@
-// src/store/store.ts
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import { 
-  persistStore, 
-  persistReducer, 
-  FLUSH, 
-  REHYDRATE, 
-  PAUSE, 
-  PERSIST, 
-  PURGE, 
-  REGISTER 
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+// store.ts
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-// Import your reducers
-import chatReducer from './chatSlice';
-import uiReducer from './uiSlice';
-import userReducer from './userSlice';
-import stationsReducer from './stationsSlice';
-import carReducer from './carSlice';
-import bookingReducer from './bookingSlice';
-import stations3DReducer from './stations3DSlice';
-import dispatchReducer from './dispatchSlice';
-import verificationReducer from './verificationSlice';
+// Import slices as usual
+import chatReducer from "./chatSlice";
+import uiReducer from "./uiSlice";
+import userReducer from "./userSlice";
+import stationsReducer from "./stationsSlice";
+import carReducer from "./carSlice";
+import bookingReducer from "./bookingSlice";
+import stations3DReducer from "./stations3DSlice";
+import dispatchReducer from "./dispatchSlice";
+import verificationReducer from "./verificationSlice";
 
-// Configuration for redux-persist
+// Import your custom transform
+import bookingStep5Transform from "./bookingStep5Transform"; // The file we created above
+
+// Example user persist config
 const userPersistConfig = {
-  key: 'user',
+  key: "user",
   storage,
-  whitelist: ['authUser', 'isSignedIn', 'defaultPaymentMethodId'], // Only persist these fields
+  whitelist: ["authUser", "isSignedIn", "defaultPaymentMethodId"],
 };
 
+// Booking persist config with the step=5 transform
 const bookingPersistConfig = {
-  key: 'booking',
+  key: "booking",
   storage,
   whitelist: [
-    'step', 
-    'stepName', 
-    'departureDate', 
-    'ticketPlan', 
-    'departureStationId', 
-    'arrivalStationId',
-    'route'
-  ], // Fields we want to persist
+    "step",
+    "stepName",
+    "departureDate",
+    "ticketPlan",
+    "departureStationId",
+    "arrivalStationId",
+    "route",
+  ],
+  transforms: [bookingStep5Transform], // <---- Use the custom transform here
 };
 
-// Create the root reducer with persistence
 const rootReducer = combineReducers({
   chat: chatReducer,
   ui: uiReducer,
@@ -57,28 +61,25 @@ const rootReducer = combineReducers({
   verification: verificationReducer,
 });
 
-// Configure the store with the persisted reducer
 export const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-        // Ignore date objects (which are non-serializable)
-        ignoredActionPaths: ['payload.departureDate'],
-        ignoredPaths: ['booking.departureDate'],
+        // If you are ignoring date objects, keep these lines
+        ignoredActionPaths: ["payload.departureDate"],
+        ignoredPaths: ["booking.departureDate"],
       },
     }),
 });
 
-// Create the persistor
 export const persistor = persistStore(store);
 
-// Types for use in components and hooks
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
